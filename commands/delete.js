6d7cb6fa0b326b62fd62db97216de9e68bd701a3
Linux123123 <linas.alexx@gmail.com>
2020-12-02 21:38:53 +0200

    Add alias

--- commands/delete.js
+++ commands/delete.js
@@ -46,7 +46,7 @@ exports.run = async (client, message, args, level) => {
 exports.conf = {
     enabled: true,
     guildOnly: true,
-    aliases: ['purge', 'clear'],
+    aliases: ['purge', 'clear', 'remove'],
     permLevel: 'Moderator',
 };
 
